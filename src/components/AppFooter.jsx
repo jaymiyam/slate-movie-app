5ad72a77d7af16e886ba095df56548bf1e220fb3
Jaymi Yam <jaymiyam@gmail.com>
2025-05-07 00:18:40 +0800

    added footer link

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function AppFooter() {
   return (
     <footer className="w-full bg-teal-950 text-white font-display text-xl text-center py-4">
-      <p>Designed & created by Jaymi.</p>
+      <p>
+        Designed & created by{' '}
+        <a href="https://github.com/jaymiyam" target="blank">
+          Jaymi.
+        </a>
+      </p>
     </footer>
   );
 }
